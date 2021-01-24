cef6f885f8ffe52653df74430038badbf70a043b
Priya-sosa-john <58131455+Priya-sosa-john@users.noreply.github.com>
2021-01-24 18:23:23 +0530

    Add files via upload

---
+++ TrustDataLayer/GothamContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrustDataLayer.Models;
+
+namespace TrustDataLayer
+{
+    public class GothamContext :DbContext
+    {
+        public GothamContext(DbContextOptions<GothamContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Admins> Admins { get; set; }
+        public DbSet<Volunteers> Volunteers { get; set; }
+        public DbSet<Foods> Foods { get; set; }
+
+        public DbSet<Outlets> Outlets { get; set; }
+    }
+
+}
---
+++ TrustDataLayer/Migrations/20210124121415_InitialMigration.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TrustDataLayer.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<long>(type: "bigint", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Outlets",
+                columns: table => new
+                {
+                    OutletName = table.Column<string>(type: "text", nullable: false),
+                    StreetName = table.Column<string>(type: "text", nullable: false),
+                    Landmark = table.Column<string>(type: "text", nullable: false),
+                    VolunteerAvailable = table.Column<int>(type: "integer", nullable: false),
+                    PacketsCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outlets", x => x.OutletName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    OutletAssign = table.Column<string>(type: "text", nullable: false),
+                    OutletName1 = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Foods_Outlets_OutletName1",
+                        column: x => x.OutletName1,
+                        principalTable: "Outlets",
+                        principalColumn: "OutletName",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Volunteers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<long>(type: "bigint", maxLength: 10, nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: false),
+                    OutletChoice = table.Column<string>(type: "text", nullable: false),
+                    OutletName1 = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Volunteers", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Volunteers_Outlets_OutletName1",
+                        column: x => x.OutletName1,
+                        principalTable: "Outlets",
+                        principalColumn: "OutletName",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Foods_OutletName1",
+                table: "Foods",
+                column: "OutletName1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Volunteers_OutletName1",
+                table: "Volunteers",
+                column: "OutletName1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Foods");
+
+            migrationBuilder.DropTable(
+                name: "Volunteers");
+
+            migrationBuilder.DropTable(
+                name: "Outlets");
+        }
+    }
+}
